04b657ce687c168d08433d38962663dbd833fe3d
DatHackerBoi <16878148+JoshekDeveloper@users.noreply.github.com>
2018-02-27 20:36:53 +0800

    The meat and potatoes
    Here it is, but no token :wink:

---
+++ bot.py
@@ -0,0 +1,43 @@
+# Arc by Joshek
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix='/')
+
+print (discord.__version__)
+
+@bot.event
+async def on_ready():
+    print ("Arc is online")
+
+# ping command
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say("Pong!")
+
+# userinfo command
+@bot.command(pass_context=True)
+async def userinfo(ctx, user: discord.Member):
+    embed = discord.Embed(title)
+    await bot.say("Username: {}".format(user.name))
+    await bot.say("User ID: {}".format(user.id))
+    await bot.say("Status: {}".format(user.status))
+    await bot.say("Roles: {}".format(user.roles))
+    await bot.say("Join date: {}".format(user.joined_at))
+
+# kick command
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member):
+    await bot.say("{} has been kicked.".format(user.name))
+    await bot.kick(user)
+
+# ban command
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member):
+    await bot.say("{} has been banned.".format(user.name))
+    await bot.ban(user)
+    
+bot.run("Token goes here")
