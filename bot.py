5a38c02915f3828308d2b4a43daa89b563ca8d56
DatHackerBoi <16878148+JoshekDeveloper@users.noreply.github.com>
2018-02-28 11:01:20 +0800

    server and about commands

--- bot.py
+++ bot.py
@@ -65,5 +65,22 @@ async def kick(ctx, user: discord.Member):
     await bot.say("{} has been banned.".format(user.name))
     await bot.ban(user)
 
+# about command
+async def about(ctx):
+    embed = discord.Embed(title="Bot made by Joshek#1337", description="Hi, I'm Arc, I'm a bot made in Discord.py by my wonderful creator, Joshek.", color=0x35FA00)
+    embed.set_footer(text="If you find any bugs, report them to my creator so he can fix them!")
+    await bot.say(embed=embed)
+    
+# server command
+async def server(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Here's the server details", color=0x00ff00)
+    embed.set_author(name="Arc")
+    embed.add_field(name="Guild Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="Guild ID", value=ctx.message.server.id, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Member count", value=len(ctx.message.server.members))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)
+
 # this is where you insert you token
 bot.run("TOKEN GOES HERE")
