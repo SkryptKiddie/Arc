7e85d17a412b881df56e7baa81798c331d0ee5de
Joshek <16878148+JoshekDeveloper@users.noreply.github.com>
2018-03-19 19:38:06 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,42 +29,44 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 
-bot = commands.Bot(command_prefix='arc!')
+
+bot = commands.Bot(command_prefix='arc!', formatter=None, pm_help=True, description='Writen by Joshek#1337', command_not_found='Invalid command!')
 print (discord.version_info)
 print (discord.__version__)
-print ("Arc is loading...")
+print ("Connecting to the Discord API...")
 
 @bot.event
 async def on_ready():
-    print ("Arc is online")
-    
+    print ("Arc is online")   
+
 # ping command
 @bot.command(pass_context=True)
 async def ping(ctx):
-    await bot.say("Pong! :ping_pong:")
+    embed = discord.Embed(title="Pong! :ping_pong:")
+    await bot.say(embed=embed)
     
-# about command
-@bot.command(pass_context=True)
-async def info(ctx):
-    await bot.say("My name is Arc and I am written in Discord.py. I was made by Joshek#1337 over the course of a few weeks. I will be getting more features soon.")
-    await bot.say("If you wish to support development, check you the code at https://github.com/JoshekDeveloper/Arc.")
-
 # invite command
 @bot.command(pass_context=True)
 async def invite(ctx):
-    await bot.say("My invite is https://discordapp.com/oauth2/authorize?client_id=417982648749654016&scope=bot&permissions=8 .")
+    embed=discord.Embed(title="Invite Arc to your server", url="https://discordapp.com/oauth2/authorize?client_id=417982648749654016&scope=bot&permissions=8", color=0x80ff00)
+    embed.set_footer(text="Written by Joshek#1337 in Discord.py")
+    await bot.say(embed=embed)  
     
-# support command
-@bot.command(pass_context=True)
-async def support(ctx):   
-    await bot.say("https://discord.gg/cTMfa56")
-
 # joined command
 @bot.command(pass_context=True)
 async def joined(ctx, member: discord.Member = None):
     if member is None:
         member = ctx.message.author
     await bot.say('{0} joined the server at {0.joined_at}'.format(member))
-    
-# this is where you insert your token
+
+# about command
+@bot.command(pass_context=True)
+async def about(ctx):
+    embed=discord.Embed(title="About Arc", description="Hi, I'm Arc. I'm a Discord bot. I am under development and will be going offline frequently as I get more features, mainly because my creator can't figure out how a development bot works.", color=0x0080c0)
+    embed.add_field(name="GitHub", value="https://github.com/JoshekDeveloper/Arc", inline=True)
+    embed.add_field(name="Support", value="https://discord.gg/cTMfa56", inline=True)
+    embed.set_footer(text="Written by Joshek#1337 in Discord.py")
+    await bot.say(embed=embed)
+
+# this is where you insert you token
 bot.run("Token goes here")
